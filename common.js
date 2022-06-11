12c656cf1dcbddcde0651d80255ea5252b025a5b
harinder <harinderyadav1008@gmail.com>
2022-06-11 13:16:49 +0530

    Added java html file, added name popup, added common js file

---
+++ common.js
@@ -0,0 +1,4 @@
+document.getElementById('item1').addEventListener('click', ()=> {
+    location.href="http://localhost:5500";
+    console.log('hatoikojk')
+})
---
+++ index.js
@@ -0,0 +1,5 @@
+// Added click functionality to buttons
+
+document.getElementById('java').addEventListener('click', ()=> {
+    location.href="http://localhost:5500/javaQuiz.html";
+})
